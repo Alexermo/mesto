7d655d4f5661ae5756cf45266a19fbf8750c8168
Alexermo <89266703634@mail.ru>
2023-05-30 17:36:32 +0300

    30.05.2023

--- scripts/index.js
+++ scripts/index.js
@@ -20,3 +20,28 @@ popup.addEventListener('click', function(evt) {
 })
 
 
+// // Находим форму в DOM
+// let formElement = // Воспользуйтесь методом querySelector()
+// // Находим поля формы в DOM
+// let nameInput = document.querySelector()
+// let jobInput = document.querySelector()
+
+// // Обработчик «отправки» формы, хотя пока
+// // она никуда отправляться не будет
+// function handleFormSubmit (evt) {
+//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+//                                                 // Так мы можем определить свою логику отправки.
+//                                                 // О том, как это делать, расскажем позже.
+
+//     // Получите значение полей jobInput и nameInput из свойства value
+
+//     // Выберите элементы, куда должны быть вставлены значения полей
+
+//     // Вставьте новые значения с помощью textContent
+// }
+
+// // Прикрепляем обработчик к форме:
+// // он будет следить за событием “submit” - «отправка»
+// formElement.addEventListener('submit', handleFormSubmit);
+
+
